ec0799364f9db647d095fc8ace5371b7e3eca097
Adam Knezevic <domacikolaci@github.com>
2015-11-03 19:46:55 +1100

    Basic game files, attempt 2

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+/*
+TODO:
+-Make level 1
+-Make basic enemy
+-Make combat system
+-Make player stats
+--Make classes
+*/
+using namespace std;
+
+int main()
+{
+    string name;
+    string answerGameStart;
+
+
+
+    cout << "What is your name adventurer?" << endl;
+    cin >> name;
+    cout << "Hello " << name << " Are you ready to set off on an adventure?" << endl;
+    cin >> answerGameStart;
+
+    if (answerGameStart == "yes")
+    {
+        cout << "ayyyyyy" << endl;
+    }
+    else
+    {
+        cout << "riiiiip" << endl;
+    }
+}
+
+
+int gameLevel1()
+{
+    //Add level 1
+}
---
+++ src/level1.cpp
@@ -0,0 +1,11 @@
+#include "level1.h"
+
+level1::level1()
+{
+    //ctor
+}
+
+level1::~level1()
+{
+    //dtor
+}
